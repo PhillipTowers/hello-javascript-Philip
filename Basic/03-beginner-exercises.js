45c6c1c80af2f2aabacf0708a3cd1c2c7ce26f6e
TK-2127 <felipetorresgutierrez1998@gmail.com>
2024-07-20 13:58:31 -0400

    Update 03-beginner-exercises.js
    ejercicios tipos de datos

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,10 +5,37 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Escribe un comentario en una línea
+// R: esto es un comentario en una linea
 
-// 2. Escribe un comentario en varias líneas
+// 2. Escribe un comentario en varias líneas, R:
+/*esto es un comentario 
+en varias lineas en Js
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let miPkmnFav = "infernape"
+let numeroTK = 2712 
+let graduado = true
+let graduado2 = false
+let indefinido = undefined
+let vacio = null
+let simbolo = Symbol("elSimbolodelMainkra")
+let knekro = BigInt(354684634568489763518796782613187687968465413216879874654313216584987654651321857978)
+let dineroKnekro = 182736128973619246129478612981736192671278946192746129741629476192461293861294169157n
+
+console.log(typeof miPkmnFav)
+console.log(typeof numeroTK)
+console.log(typeof graduado)
+
+console.log(typeof graduado2)
+console.log(typeof indefinido)
+
+console.log(typeof vacio)
+console.log(typeof simbolo)
+
+console.log(typeof knekro)
+
+console.log(typeof dineroKnekro)
 
 // 4. Imprime por consola el valor de todas las variables
 
